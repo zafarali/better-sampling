Plotting improvements, fix small glitches in data calculation, all methods run for the same number of time frames now

RVISampler.py
@@ -47,7 +47,10 @@ class RVISampler(Sampler):
             log_proposal_prob = np.zeros((stochastic_process.n_agents, 1))
             policy_gradient_trajectory_info = MultiTrajectory(stochastic_process.n_agents)
             # go in reverse time:
-            for t in reversed(range(0, stochastic_process.T-1)):
+            # for t in reversed(range(0, stochastic_process.T-1)):
+            t = stochastic_process.T
+            while True:
+                if stochastic_process.global_time == 0: break
                 # draw a reverse step
                 # this is p(w_{t} | w_{t+1})
                 assert len(x_tm1.size()) == 2
@@ -92,6 +95,7 @@ class RVISampler(Sampler):
                     policy_gradient_trajectory_info.append(x_tm1, action, reward, value_estimate, log_prob_action, x_t, done)
 
                 x_tm1 = x_t
+                t -= 1
 
             # print(trajectory_i)
             # conduct an update step.
@@ -123,10 +127,10 @@ class RVISampler(Sampler):
 
 
             if feed_time:
-                trajectory_i = np.hstack(trajectory_i).reshape(stochastic_process.n_agents, stochastic_process.T,
+                trajectory_i = np.hstack(trajectory_i).reshape(stochastic_process.n_agents, stochastic_process.T+1,
                                                                x_t.size()[-1]-1)
             else:
-                trajectory_i = np.hstack(trajectory_i).reshape(stochastic_process.n_agents, stochastic_process.T,
+                trajectory_i = np.hstack(trajectory_i).reshape(stochastic_process.n_agents, stochastic_process.T+1,
                                                                x_t.size()[-1])
             likelihood_ratios = log_path_prob - log_proposal_prob
             selected_trajectories = np.where(log_path_prob > -np.inf) # TODO: find a way to make it less excplicit to throw away

Samplers.py
@@ -33,7 +33,7 @@ class ABCSampler(Sampler):
             trajectory = stochastic_process.simulate(self.rng)
             final_position = trajectory[-1]
             # only accept this trajectory if it ended close to the final
-            if np.sum(np.abs(observed_ending_location - final_position)) < self.tolerance:
+            if np.sum(np.abs(observed_ending_location - final_position)) <= self.tolerance:
                 trajectories.append(trajectory)
             all_trajectories.append(trajectory)
 
@@ -117,13 +117,17 @@ class ISSampler(Sampler):
             log_path_prob = 0
             log_proposal_prob = 0
             # go in reverse time:
-            for t in reversed(range(0, stochastic_process.T-1)):
+            t = stochastic_process.T
+            while True:
                 x_t = trajectory_i[-1]
                 # draw a reverse step
                 # this is p(w_{t} | w_{t+1})
-                step_idx, step, log_prob_proposal_step = proposal.draw(x_t, t)
-                # print('proposal_log_prob step:',log_prob_proposal_step)
-                x_t, path_log_prob, _, _ = stochastic_process.step(step_idx)
+                try:
+                    step_idx, step, log_prob_proposal_step = proposal.draw(x_t, t)
+                    # print('proposal_log_prob step:',log_prob_proposal_step)
+                    x_t, path_log_prob, _, _ = stochastic_process.step(step_idx)
+                except TimeoutError:
+                    break
                 # print('path_log_prob step:',path_log_prob)
 
                 # probability of the path gets updated:
@@ -131,6 +135,7 @@ class ISSampler(Sampler):
                 log_proposal_prob += log_prob_proposal_step
                 # take the reverse step:
                 trajectory_i.append(x_t)
+                t-=1
 
 
             likelihood_ratio = log_path_prob - log_proposal_prob

analytic_posterior.py
@@ -1,5 +1,6 @@
 from scipy.misc import comb
 import numpy as np
+import matplotlib.pyplot as plt
 
 class AnalyticPosterior(object):
     def pdf(self, x, d):
@@ -52,8 +53,26 @@ class TwoStepRandomWalkPosterior(AnalyticPosterior):
         """
         c, T, p = self.c, self.T, self.p
         term = 0.5 * (d-x+T)
-        if not (x in range(-c, c, 1) and term in range(0, T)):
+        if not (x in range(-c, c+1, 1) and term in range(0, T+1)):
             return 0
         else:
             return (1/(2*c))*comb(T, term)*(p**term)*((1-p)**(T-term))
 
+
+    def plot(self, observed_d, ax=None, **kwargs):
+        if ax is None:
+            ax = plt.gca()
+        prior_domain = np.arange(-self.c-2, self.c+2)
+        pdf = np.array([self.pdf(x, observed_d) for x in prior_domain])
+        pdf /= pdf.sum()
+        ax.scatter(prior_domain, pdf, **kwargs)
+        ax.set_xlabel(r"$x_0$")
+        ax.set_ylabel('Probability')
+        ax.set_title('Posterior Distribution')
+        # ax.set_xticklabels(prior_domain)
+        return ax
+
+    def expectation(self, observed_d):
+        pdf = np.array([self.pdf(x, observed_d) for x in np.arange(-self.c, self.c+1)])
+        pdf /= pdf.sum()
+        return np.sum(np.arange(-self.c, self.c+1) * pdf)

utils.py
@@ -6,13 +6,14 @@ class SamplingResults(object):
     """
     An abstraction on the results obtained
     """
-    def __init__(self, sampler_name, true_trajectory):
+    def __init__(self, sampler_name, true_trajectory, histbin_range=None):
         self.sampler_name = sampler_name
         self.true_trajectory = true_trajectory
         self._all_trajectories = None
         self._trajectories = None
         self._posterior_particles = None
         self._posterior_weights = None
+        self._histbin_range = histbin_range
 
     def all_trajectories(self, trajectories=None):
         """
@@ -42,8 +43,9 @@ class SamplingResults(object):
         :param posterior_particles:
         :return:
         """
-        if posterior_particles is None:
+        if posterior_particles is not None:
             assert self._posterior_particles is None
+            self._posterior_particles = posterior_particles
             return self._posterior_particles
         else:
             self._posterior_particles = posterior_particles
@@ -54,8 +56,9 @@ class SamplingResults(object):
         :param posterior_weights:
         :return:
         """
-        if posterior_weights is None:
+        if posterior_weights is not None:
             assert self._posterior_weights is None
+            self._posterior_weights = posterior_weights
             return self._posterior_weights
         else:
             self._posterior_weights = posterior_weights
@@ -111,12 +114,17 @@ class SamplingResults(object):
         else:
             return np.mean((posterior_weights*posterior_particles - expected_value)**2)
 
-
-    def plot_distribution(self, ax=None):
+    def plot_distribution(self, histbin_range=None, ax=None, **kwargs):
         if ax is None:
             ax = plt.gca()
 
-        ax.hist(np.array(self._posterior_particles).reshape(-1), bins=np.arange(-4, 5)+0.5,weights=np.array(self._posterior_weights).reshape(-1))
+        _histbin_range = self._histbin_range if not histbin_range else histbin_range
+
+        ax.hist(np.array(self._posterior_particles).reshape(-1),
+                normed=True,
+                bins=np.arange(-_histbin_range-1, _histbin_range+2)+0.5,
+                weights=np.array(self._posterior_weights).reshape(-1),
+                **kwargs)
         ax.set_xlabel('x_0')
         ax.set_ylabel('Frequency')
         ax.set_title('Histogram of trajectory starting positions')