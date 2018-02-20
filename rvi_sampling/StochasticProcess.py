Fix inconsistencies in states and transitions for samplers and fix KL divergence bug, add tests

rvi_sampling/StochasticProcess.py
@@ -87,7 +87,8 @@ class RandomWalk(StochasticProcess):
         if rng is None:
             rng = self.rng
         x0 = self.prior.rvs()
-        steps_idx = rng.multinomial(1, self.step_probs, self.T).argmax(axis=1)
+        # T steps is T-1 transitions
+        steps_idx = rng.multinomial(1, self.step_probs, self.T-1).argmax(axis=1)
         steps_taken = np.take(self.step_sizes, steps_idx, axis=0)
         steps_taken = np.vstack([x0, steps_taken])
         return steps_taken.cumsum(axis=0)
@@ -97,7 +98,7 @@ class RandomWalk(StochasticProcess):
         This will reset the locations of all the agents who are currently interacting with
         the stochastic process
         """
-        self.global_time = self.T
+        self.transitions_left = self.T-1
         self.x_agent = np.repeat(self.xT.reshape(1, self.dimensions), self.n_agents, axis=0)
 
     def reset(self):
@@ -122,7 +123,7 @@ class RandomWalk(StochasticProcess):
         :param actions: the array with the actions to be executed by each agent
         :param reverse: defines if the step execution is going in reverse or not
         """
-        if self.global_time == 0:
+        if self.transitions_left == 0:
             #TODO return a custom error here to not conflate it with builtin types.
             raise TimeoutError('You have already reached the end of the episode. Use reset()')
 
@@ -131,11 +132,13 @@ class RandomWalk(StochasticProcess):
 
         reversal_param = -1 if reverse else +1
         self.x_agent = self.x_agent + (steps_taken * reversal_param)
-        self.global_time -= 1
-        if self.global_time == 0:
-            step_log_probs = np.log(self.prior.pdf(self.x_agent))
+        self.transitions_left -= 1
+        if self.transitions_left == 0:
+            # if there are no more transitions left, also add the "final reward"
+            # to the step_log_probs
+            step_log_probs += np.log(self.prior.pdf(self.x_agent))
 
-        return (self.x_agent, step_log_probs.reshape(-1, 1), self.global_time == 0, {})
+        return (self.x_agent, step_log_probs.reshape(-1, 1), self.transitions_left == 0, {})
 
 
 class PyTorchWrap(object):
@@ -147,7 +150,6 @@ class PyTorchWrap(object):
         self.dimensions = stochastic_process.dimensions
         self.step_sizes = stochastic_process.step_sizes
         self.x0 = stochastic_process.x0
-        # self.state = stochastic_process.state
         self.state_space = stochastic_process.state_space
         self.action_space = stochastic_process.action_space
         self.use_cuda = use_cuda
@@ -160,8 +162,8 @@ class PyTorchWrap(object):
     def train_mode(self, mode):
         self._training = mode
     @property
-    def global_time(self):
-        return self.stochastic_process.global_time
+    def transitions_left(self):
+        return self.stochastic_process.transitions_left
 
     def variable_wrap(self, tensor):
         if not isinstance(tensor, Variable):

rvi_sampling/distributions/analytic_posterior.py
@@ -42,7 +42,7 @@ class TwoStepRandomWalkPosterior(AnalyticPosterior):
         """
         self.c = c
         self.p = p
-        self.T = T
+        self.T = T-1 # T steps is T-1 transitions
 
     def pdf(self, x, d):
         """
@@ -77,16 +77,23 @@ class TwoStepRandomWalkPosterior(AnalyticPosterior):
         pdf /= pdf.sum()
         return np.sum(np.arange(-self.c, self.c+1) * pdf)
 
-    def kl_divergence(self, estimated_dist, observed_d):
+    def kl_divergence(self, estimated_dist, observed_d, verbose=False):
         KL_true_est = 0
         KL_est_true = 0
 
         analytic_probs = np.array([float(self.pdf(i, observed_d)) for i in estimated_dist.keys()])
         analytic_probs /= analytic_probs.sum()
         analytic_probs = dict(zip(estimated_dist.keys(), analytic_probs))
-        # KL is only defined when p>0 and q>0 or both zero
-        support_check = [ (p>0 and q>0) or (p==0 and q==0) for p, q in zip(analytic_probs.values(), estimated_dist.values())]
 
+        # check if the same values are there in the support
+        support_check = [set(analytic_probs.keys()) == set(estimated_dist.keys())]
+
+        # KL is only defined when p>0 and q>0 or both zero
+        support_check += [ (analytic_probs[k]>0 and estimated_dist[k]>0) or (analytic_probs[k]==0 and estimated_dist[k]==0) for k in analytic_probs.keys() ]
+
+        if verbose: print('analytic:',analytic_probs)
+        if verbose: print('estimated',estimated_dist)
+        if verbose: print('support check',support_check)
         if not all(support_check):
             return (np.nan, np.nan)
 

rvi_sampling/samplers/RVISampler.py
@@ -66,17 +66,15 @@ class RVISampler(Sampler):
                 # so that the learner has access to it.
                 x_with_time = torch.zeros(stochastic_process.n_agents, x_tm1.size()[-1]+1)
                 x_with_time[:, :x_tm1.size()[-1]].copy_(x_tm1.data)
-                x_with_time[:, x_tm1.size()[-1]].copy_(torch.ones(stochastic_process.n_agents))
+                x_with_time[:, x_tm1.size()[-1]].copy_(torch.ones(stochastic_process.n_agents)) # 100% of the time is left
                 x_tm1 = Variable(x_with_time)
 
             log_path_prob = np.zeros((stochastic_process.n_agents, 1))
             log_proposal_prob = np.zeros((stochastic_process.n_agents, 1))
             policy_gradient_trajectory_info = MultiTrajectory(stochastic_process.n_agents)
-            # go in reverse time:
-            # for t in reversed(range(0, stochastic_process.T-1)):
-            t = stochastic_process.T
-            while True:
-                if stochastic_process.global_time == 0: break
+
+            done = torch.IntTensor([0])
+            while not np.all(done.numpy()):
                 # draw a reverse step
                 # this is p(w_{t} | w_{t+1})
                 assert len(x_tm1.size()) == 2
@@ -84,13 +82,9 @@ class RVISampler(Sampler):
                 # print('proposal_log_prob step:',log_prob_proposal_step)
                 x_t, path_log_prob, done, _ = stochastic_process.step(action, reverse=False)
 
-                if stochastic_process.global_time != stochastic_process.T:
-                    # as long as this is not the first transition provide an "instant reward"
-                    reward_ = path_log_prob.float().view(-1,1) - log_prob_action.data.float().view(-1, 1)
-                else:
-                    reward_ = path_log_prob.float().view(-1, 1)
 
-                # print(reward)
+                reward_ = path_log_prob.float().view(-1,1) - log_prob_action.data.float().view(-1, 1)
+
                 reward = torch.zeros_like(reward_)
                 reward.copy_(reward_)
                 reward[reward <= -np.inf] = -100000. # throw away infinite negative rewards
@@ -112,7 +106,7 @@ class RVISampler(Sampler):
                     # so that the learner has access to it.
                     x_with_time = torch.zeros(stochastic_process.n_agents, x_tm1.size()[-1])
                     x_with_time[:, :x_tm1.size()[-1]-1].copy_(x_t.data)
-                    x_with_time[:, x_tm1.size()[-1]-1].copy_((t/stochastic_process.T) * torch.ones(stochastic_process.n_agents))
+                    x_with_time[:, x_tm1.size()[-1]-1].copy_((stochastic_process.transitions_left/(stochastic_process.T-1)) * torch.ones(stochastic_process.n_agents))
                     x_t = Variable(x_with_time)
                     assert x_tm1.size() == x_t.size(), 'State sizes must match, but they dont. {} != {}'.format(x_tm1.size(), x_t.size())
                     policy_gradient_trajectory_info.append(x_tm1[:, :-1], action, reward, value_estimate,
@@ -121,11 +115,12 @@ class RVISampler(Sampler):
                     policy_gradient_trajectory_info.append(x_tm1, action, reward, value_estimate, log_prob_action, x_t, done)
 
                 x_tm1 = x_t
-                t -= 1
+
                 # endwhile loop
 
             policy_gradient_trajectory_info.torchify()
 
+            # update the proposal distribution
             if self._training:
                 returns = gradients.calculate_returns(policy_gradient_trajectory_info.rewards, 1, None)
                 advantages = returns - policy_gradient_trajectory_info.values
@@ -154,13 +149,15 @@ class RVISampler(Sampler):
 
 
             if feed_time:
-                trajectory_i = np.hstack(trajectory_i).reshape(stochastic_process.n_agents, stochastic_process.T+1,
+                trajectory_i = np.hstack(trajectory_i).reshape(stochastic_process.n_agents, stochastic_process.T,
                                                                x_t.size()[-1]-1)
             else:
-                trajectory_i = np.hstack(trajectory_i).reshape(stochastic_process.n_agents, stochastic_process.T+1,
+                trajectory_i = np.hstack(trajectory_i).reshape(stochastic_process.n_agents, stochastic_process.T,
                                                                x_t.size()[-1])
+
+            # select paths for storage
             likelihood_ratios = log_path_prob - log_proposal_prob
-            selected_trajectories = np.where(log_path_prob > -np.inf) # TODO: find a way to make it less excplicit to throw away
+            selected_trajectories = np.where(log_path_prob > -np.inf)
             for traj_idx in selected_trajectories[0]:
                     trajectories.append(trajectory_i[traj_idx, ::-1, :stochastic_process.dimensions])
                     posterior_particles.append(trajectories[-1][0])

rvi_sampling/samplers/Samplers.py
@@ -71,17 +71,14 @@ class MCSampler(Sampler):
             log_path_prob = 0
 
             # go in reverse time:
-            # for t in reversed(range(0, stochastic_process.T-1)):
-            while True:
+            done = False
+            while not done:
                 x_t = trajectory_i[-1]
                 # draw a reverse step
                 # this is p(w_{t} | w_{t+1})
-                try:
-                    step_idx, step, proposal_log_prob = self.draw_step(x_t)
-                    x_t, path_log_prob, _, _ = stochastic_process.step(step_idx, reverse=False)
-                except TimeoutError:
-                    # print('Ended at {}.'.format(x_t))
-                    break
+                step_idx, step, proposal_log_prob = self.draw_step(x_t)
+                x_t, path_log_prob, done, _ = stochastic_process.step(step_idx, reverse=False)
+
                 # probability of the path gets updated:
                 log_path_prob += path_log_prob
                 # take the reverse step:
@@ -89,6 +86,7 @@ class MCSampler(Sampler):
 
             if log_path_prob > -np.inf:
                 trajectories.append(np.vstack(list(reversed(trajectory_i))))
+
             all_trajectories.append(np.vstack(list(reversed(trajectory_i))))
 
         results.all_trajectories(all_trajectories)
@@ -125,31 +123,26 @@ class ISSampler(Sampler):
             log_path_prob = 0
             log_proposal_prob = 0
             # go in reverse time:
-            t = stochastic_process.T
-            while True:
+            done = False
+            while not done:
                 x_t = trajectory_i[-1]
                 # draw a reverse step
                 # this is p(w_{t} | w_{t+1})
-                try:
-                    step_idx, step, log_prob_proposal_step = proposal.draw(x_t, t)
-                    # print('proposal_log_prob step:',log_prob_proposal_step)
-                    x_t, path_log_prob, _, _ = stochastic_process.step(step_idx, reverse=False)
-                except TimeoutError:
-                    break
-                # print('path_log_prob step:',path_log_prob)
+                step_idx, step, log_prob_proposal_step = proposal.draw(x_t, stochastic_process.transitions_left)
+                # print('proposal_log_prob step:',log_prob_proposal_step)
+                x_t, path_log_prob, done, _ = stochastic_process.step(step_idx, reverse=False)
 
-                # probability of the path gets updated:
+
+                # accumulate log probs of the path and the proposal:
                 log_path_prob += path_log_prob
                 log_proposal_prob += log_prob_proposal_step
-                # take the reverse step:
+
                 trajectory_i.append(x_t)
-                t-=1
 
 
             likelihood_ratio = log_path_prob - log_proposal_prob
 
             if log_path_prob > -np.inf:
-                # print()
                 trajectories.append(np.vstack(list(reversed(trajectory_i))))
                 posterior_particles.append(trajectories[-1][0])
                 posterior_weights.append(np.exp(likelihood_ratio))