Fix baseline bug, will be merged from zafarali/policy-gradient-methods#918d268d999e3cd5775c71c3ef67fb3b22706869

experiments/method_comparison_experiment.py
@@ -91,7 +91,7 @@ if __name__=='__main__':
     rw = create_rw()
     # create a policy for the RVI sampler
     fn_approximator = MLP_factory(DIMENSIONS+int(FEED_TIME),
-                                  hidden_sizes=[32, 32],
+                                  hidden_sizes=[16, 16],
                                   output_size=len(POSSIBLE_STEPS),
                                   hidden_non_linearity=nn.ReLU)
     policy = MultinomialPolicy(fn_approximator)