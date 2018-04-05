ee6e428943a8ba2fcd1eea86ca3d1812bece4d8e
zafarali <zafarali.ahmed@gmail.com>
2018-04-05 09:38:35 -0400

    Add custom summarizer

--- experiments/KL_summarizer.py
+++ experiments/KL_summarizer.py
@@ -20,6 +20,24 @@ sns.set_style('whitegrid')
 sns.set_color_codes('colorblind')
 sns.set_context('paper', font_scale=1.5)
 
+samplers = ['ISSampler', 'ABCSampler', 'MCSampler', 'RVISampler']
+colors = sns.color_palette('colorblind', len(samplers))
+
+def custom_time_series(timeseries_files, ax):
+    colors = sns.color_palette('colorblind', 4)
+    for color, sampler_name in zip(colors, samplers):
+        for y, x in zip(timeseries_files[sampler_name]['KL'], timeseries_files[sampler_name]['time']):
+            ax.plot(x,y, color=color, alpha=0.1)
+
+        ax.plot(np.mean(timeseries_files[sampler_name]['time'], axis=0),
+                np.mean(timeseries_files[sampler_name]['KL'], axis=0), color=color)
+
+    from matplotlib.lines import Line2D
+    custom_lines = []
+    for color in colors:
+        custom_lines.append(Line2D([0], [0], color=color, linestyle='-'))
+    ax.legend(custom_lines, ['ISSampler', 'ABCSampler', 'MCSampler', 'RVISampler'])
+    ax.semilogy()
 
 def main(args):
     kl_values = defaultdict(lambda: [])
@@ -51,9 +69,19 @@ def main(args):
     ax = fig.add_subplot(1, 1, 1)
     for sampler_name in kl_values.keys():
         sns.kdeplot(np.array(kl_values[sampler_name]), shade=True, label=sampler_name, ax=ax)
+    ax.set_xlabel('KL')
+    ax.set_ylabel('Density')
     ax.set_xlim(0)
     ax.legend(fontsize='x-small')
-    fig.savefig(os.path.join(args.out_folder, 'KL_KDE.pdf'))
+    fig.savefig(os.path.join(args.out_folder, args.name+'KL_KDE.pdf'))
+
+    f = plt.figure()
+    ax = f.add_subplot(1, 1, 1)
+    custom_time_series(time_series, ax)
+    ax.set_xlabel('Time')
+    ax.set_ylabel('KL')
+    ax.set_ylim(0)
+    f.savefig(os.path.join(args.out_folder, args.name+'KL_timeseries_custom.pdf'))
 
     time_series_dfs = pd.concat(time_series_dfs, ignore_index=True)
     f = plt.figure()
@@ -62,11 +90,12 @@ def main(args):
     ax.semilogy()
     ax.set_ylim(0)
     ax.legend(fontsize='x-small')
-    f.savefig(os.path.join(args.out_folder, 'KL_timeseries.pdf'))
+    f.savefig(os.path.join(args.out_folder, args.name+'KL_timeseries.pdf'))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('Aggregate KLs from experimental runs and creates a histogram')
     parser.add_argument('-f', '--folder', help='Folder with the experimental runs', required=True, nargs='+')
     parser.add_argument('-of', '--out_folder', help='Folder to save plots in', required=True)
+    parser.add_argument('-n', '--name', help='name to append', required=False, default='summarized_')
     args = parser.parse_args()
     main(args)
