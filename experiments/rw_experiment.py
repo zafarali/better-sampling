dde4158419e6d28c52bf390559061757d02a99dc
arjun23496 <arjun.k018@gmail.com>
2018-05-14 11:36:59 -0400

    Function Approximator baseline
    This also includes a command line switch to give parameters of baseline function approximator using neural network

--- experiments/rw_experiment.py
+++ experiments/rw_experiment.py
@@ -11,6 +11,7 @@ from rvi_sampling.samplers import ISSampler, ABCSampler, MCSampler, RVISampler
 from rvi_sampling.distributions.proposal_distributions import SimonsSoftProposal, FunnelProposal
 from rvi_sampling import utils
 from pg_methods.baselines import MovingAverageBaseline
+from pg_methods.baselines import FunctionApproximatorBaseline
 from pg_methods.policies import MultinomialPolicy
 from pg_methods.networks import MLP_factory
 from pg_methods.objectives import PolicyGradientObjective
@@ -44,7 +45,15 @@ if __name__=='__main__':
         policy = MultinomialPolicy(fn_approximator)
         policy_optimizer = torch.optim.RMSprop(fn_approximator.parameters(),lr=args.learning_rate)
 
-    baseline = MovingAverageBaseline(args.baseline_decay)
+    if args.baseline_type == 'moving_average':
+        baseline = MovingAverageBaseline(args.baseline_decay)
+    else:
+        baseline_fn_approximator = MLP_factory(DIMENSIONS+int(args.notime),
+                                               hidden_sizes=args.baseline_neural_network,
+                                               output_size=1,
+                                               hidden_non_linearity=nn.ReLU)
+        baseline_optimizer = torch.optim.RMSprop(policy.fn_approximator.parameters(),lr=args.learning_rate)
+        baseline = FunctionApproximatorBaseline(baseline_fn_approximator, baseline_optimizer)
 
     push_toward = [-args.rw_width, args.rw_width]
     if args.IS_proposal == 'soft':
