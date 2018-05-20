aa05a7e9684324c154040267dc49f0abfbe1c039
zafarali <zafarali.ahmed@gmail.com>
2018-05-20 12:13:20 -0400

    Reorganize experiment

--- experiments/cc_scripts/rwwidth5/cycle_mc5k_train1k_rw5.sh
+++ experiments/cc_scripts/rwwidth5/cycle_mc5k_train1k_rw5.sh
@@ -4,8 +4,8 @@
 #SBATCH --ntasks=3
 #SBATCH -o ./out_%x.txt
 #SBATCH -e ./err_%x.txt
-#SBATCH --mem-per-cpu=8G
-#SBATCH --array=0-5
+#SBATCH --mem-per-cpu=4G
+#SBATCH --array=0-4
 
 cd $SLURM_SUBMIT_DIR
 
@@ -20,16 +20,16 @@ MC_SAMPLES=5000
 TRAIN_STEPS=1000
 RW_TIME=50
 RW_WIDTH=5
-OUTFOLDER="./cycle_mc5k_train1k_rw5"
+OUTFOLDER="./cycle_experiments/cycle_mc5k_train1k_rw5"
 REWARDCLIP=-10
 
-python rvi_cycle_investigation.py --cycles $CYCLES --rw_seed 0 --rw_time $RW_TIME --rw_width $RW_WIDTH \
+python ./cycle_experiments/rvi_cycle_investigation.py --cycles $CYCLES --rw_seed 0 --rw_time $RW_TIME --rw_width $RW_WIDTH \
 -samseed $SLURM_ARRAY_TASK_ID -s $MC_SAMPLES --train_steps $TRAIN_STEPS -name rwseed0 --outfolder $OUTFOLDER \
 -rewardclip $REWARDCLIP &
-python rvi_cycle_investigation.py --cycles $CYCLES --rw_seed 2 --rw_time $RW_TIME --rw_width $RW_WIDTH \
+python ./cycle_experiments/rvi_cycle_investigation.py --cycles $CYCLES --rw_seed 2 --rw_time $RW_TIME --rw_width $RW_WIDTH \
 -samseed $SLURM_ARRAY_TASK_ID -s $MC_SAMPLES --train_steps $TRAIN_STEPS -name rwseed2 --outfolder $OUTFOLDER \
 -rewardclip $REWARDCLIP &
-python rvi_cycle_investigation.py --cycles $CYCLES --rw_seed 7 --rw_time $RW_TIME --rw_width $RW_WIDTH \
+python ./cycle_experiments/rvi_cycle_investigation.py --cycles $CYCLES --rw_seed 7 --rw_time $RW_TIME --rw_width $RW_WIDTH \
 -samseed $SLURM_ARRAY_TASK_ID -s $MC_SAMPLES --train_steps $TRAIN_STEPS -name rwseed7 --outfolder $OUTFOLDER \
 -rewardclip $REWARDCLIP &
 wait
