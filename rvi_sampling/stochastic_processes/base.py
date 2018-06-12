Enable gradient fix

rvi_sampling/stochastic_processes/base.py
@@ -63,7 +63,7 @@ class PyTorchWrap(object):
 
     def variable_wrap(self, tensor):
         with torch.set_grad_enabled(self._training):
-            tensor = tensor
+            tensor = tensor.clone()
 
         return tensor.float()
 