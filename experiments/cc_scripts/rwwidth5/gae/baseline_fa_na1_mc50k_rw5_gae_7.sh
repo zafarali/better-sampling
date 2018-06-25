287664022aa8ec0bb86cd0d13b05c006a8ab1e05
arjun23496 <arjun.k018@gmail.com>
2018-06-25 00:19:33 -0400

    script modifications [skip ci]

--- experiments/cc_scripts/rwwidth5/gae/baseline_fa_na1_mc50k_rw5_gae_7.sh
+++ experiments/cc_scripts/rwwidth5/gae/baseline_fa_na1_mc50k_rw5_gae_7.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --time=2:30:00
+#SBATCH --time=48:00:00
 #SBATCH --job-name=baseline_fa_na1_mc50k_rw5_gae_7
 #SBATCH --ntasks=3
 #SBATCH -o ./out_%x.txt
@@ -15,7 +15,7 @@ module load cuda/8.0.44
 module load cudnn/7.0
 module load qt
 
-CYCLES=50
+CYCLES=100
 MC_SAMPLES=1000
 N_AGENTS=1
 RW_TIME=50
