dbad61d436ae1c279d0dd14bc5854285dcdbef03
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2018-11-09 12:16:28 -0500

    Update setup script.

--- setup_venv.sh
+++ setup_venv.sh
@@ -1,10 +1,15 @@
+#!/usr/bin/env bash
+# How to run: PATH_TO_VENV="/path/to/venv" bash setup_venv.sh
 module load cuda/8.0.44
 module load cudnn/7.0
 module load qt
 module load python/3.6
 
-virtualenv ~/projects/def-dprecup/zafarali/venvs/RVI
-source ~/projects/def-dprecup/zafarali/venvs/RVI
+[ -z "$PATH_TO_VENV" ] && echo "Please provide PATH_TO_VENV"; exit 1;
+
+
+virtualenv $PATH_TO_VENV
+source $PATH_TO_VENV/bin/activate
 
 pip install numpy scipy --no-index
 pip install matplotlib seaborn pandas --no-index
@@ -17,3 +22,6 @@ pip install test_tube
 pip install pytest==3.0.7
 
 python3 -m pytest ./tests/
+
+echo "virtual environemtn has been setup at"
+echo $PATH_TO_VENV
