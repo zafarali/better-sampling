0f881b0f093fbfd3fafa6bfbc4dcdaea96e3ae64
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2018-11-12 23:54:55 -0500

    Fix ending point evaluation.

--- experiments/tt_hyperparameter_search.py
+++ experiments/tt_hyperparameter_search.py
@@ -9,18 +9,38 @@ from mlresearchkit.computecanada import parsers as cc_parser
 
 from rvi_sampling.utils import parsers as rvi_parser
 from rvi_sampling.utils import io as rvi_io
+from rvi_sampling.utils import common as common_utils
+from rvi_sampling.utils import stochastic_processes
+
 
 def run_rvi(args):
     # Use Slurm task ID as the environment variable.
 
     sampler_seed = os.getenv('SLURM_ARRAY_TASK_ID', args.sampler_seed)
+
+    # Run RVI for different endpoints.
+    if args.rw_time == 10:
+        end_points = [2, 4, 6, 8]
+    elif args.rw_time == 50:
+        end_points = [2, 10, 20, 30, 40, 48]
+    else:
+        raise ValueError('Unknown rw_time.')
+
+    for end_point in end_points:
+        run_rvi_experiment(args, sampler_seed, end_point)
+
+def run_rvi_experiment(args, sampler_seed, end_point):
+    common_utils.set_global_seeds(sampler_seed)
+
+    # this is where the rvi experiment actually runs.
     save_dir = os.path.join(
         args.save_dir_template.format(
             scratch=os.getenv('SCRATCH', './'),
             experiment_name='testing_experiment',
             learning_rate=args.learning_rate,
             gave_value=args.gae_value),
-        'Seed{}'.format(sampler_seed)
+        'Seed{}'.format(sampler_seed),
+        'EndPoint{}'.format(end_point)
     )
 
     folder_name = rvi_io.create_folder_name(
@@ -30,7 +50,8 @@ def run_rvi(args):
     rvi_io.argparse_saver(
         os.path.join(folder_name, 'args.txt'), args)
 
-    print(args)
+    rw, analytic = stochastic_processes.create_rw(args, biased=False)
+
 
 if __name__ == '__main__':
     parser = argparse_hopt.HyperOptArgumentParser(
@@ -42,6 +63,7 @@ if __name__ == '__main__':
     parser.add_argument(
         '--save_dir_template',
         default=('/{scratch}'
+                 '/rvi'
                  '/{experiment_name}'
                  '/lr{learning_rate}'
                  '/gae{gae_value}')
@@ -73,6 +95,7 @@ if __name__ == '__main__':
         hyperparam_optimizer=hyperparams,
         log_path=os.path.join(
             os.getenv('SCRATCH', './'),
+            'rvi',
             hyperparams.experiment_name),
         python_cmd='python3',
         test_tube_exp_name=hyperparams.experiment_name,
@@ -103,7 +126,7 @@ if __name__ == '__main__':
     cluster.per_experiment_nb_cpus = 2
     cluster.memory_mb_per_node = 16384
     cluster.optimize_parallel_cluster_cpu(
-        run_rvi,
+        run_rvi_experiment,
         nb_trials=24,
         job_name='first_tt_job',
         job_display_name='short_name')
